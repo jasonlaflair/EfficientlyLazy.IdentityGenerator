67d9817bddf777cfd7cc1a57e18e5fe58d44481d
Jason LaFlair <jason@laflair.net>
2012-11-13 16:52:25 -0600

    Updated/Fixed strong name key

--- src/EfficientlyLazy.IdentityGenerator/Properties/AssemblyInfo.cs
+++ src/EfficientlyLazy.IdentityGenerator/Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 
 [assembly: Guid("1a8a146c-e1bf-485d-9359-b82c991b1954")]
-[assembly: InternalsVisibleTo("EfficientlyLazy.IdentityGenerator.Tests")]
+[assembly: InternalsVisibleTo("EfficientlyLazy.IdentityGenerator.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c19eb6ffd4fab042f9a247807285be7d274cbcdd3408de22e1b1fabf69d84989f64e485782be42276bc0d56de25cc2ed240605257eb11eccf9e24ba95c3796cf606173b910109ffe4dbe587b5ff8d0f8bf5e4a260ab544e8b4e7643542e2cb6077c74aceb10aca8870cb41152d5f012c2e3f0f95cb8f841f46c52e41fccd9fda")]
