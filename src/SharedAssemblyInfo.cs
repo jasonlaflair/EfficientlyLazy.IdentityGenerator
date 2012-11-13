746898cec78299f6255f354caa2b3862528cbd87
Jason LaFlair <jason@laflair.net>
2012-11-13 16:11:19 -0600

    Documentation, build script updates and strong naming

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.9.5.0")]
 [assembly: AssemblyFileVersion("0.9.5.0")]
 
-[assembly: AssemblyInformationalVersionAttribute("0.9.5.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.9.5.0 (Release)")]
